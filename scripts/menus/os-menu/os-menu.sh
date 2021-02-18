a79378caa8e5a2e00c84930e27ad065f7d96719c
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-02-18 16:37:23 -0500

    Add the script/config behind the os-menu module

---
+++ scripts/menus/os-menu/os-menu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+activeSessionId=$(for sessionid in $(loginctl list-sessions --no-legend | awk '{ print $1 }')
+  do loginctl show-session -p Id -p Name -p User -p State -p Type -p Remote $sessionid | sort
+  done |
+  awk -F = '/Id/ { id = $2 } /State/ { state = $2 } /Type/ { type = $2 } /Remote/ { remote = $2 } /User/ && remote == "no" && state == "active" && (type == "x11" || type == "wayland") { print id }')
+
+
+option0="About"
+option1="Restart"
+option2="Shutdown"
+option3="Logout of $(loginctl show-session -p Name $activeSessionId | awk -F = '/Name/ { name = $2 } /Name/ { print name }')"
+
+options="$option0\n$option1\n$option2\n$option3"
+
+logoutActive() {
+  activeDesktop=$(loginctl show-session -p Desktop $activeSessionId | awk -F = '/Desktop/ { de = $2 } /Desktop/ { print de }')
+  pkill $activeDesktop
+}
+
+menuSelected="$(echo -e "$options" | rofi -dmenu -p "Arch" -config $HOME/scripts/menus/os-menu/os-menu-rofi.rasi \
+  -i -hide-scrollbar)"
+
+case $menuSelected in
+  $option0)
+    alacritty --option font.size=8.5 -e sh -c 'neofetch; while true; do sleep 1; done'
+    ;;
+  $option1)
+    echo "restart"
+    ;;
+  $option2)
+    echo "shutdown"
+    ;;
+  $option3)
+    logoutActive
+    ;;
+esac
