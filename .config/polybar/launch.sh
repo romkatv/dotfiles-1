3ac22b690c1946c2cbbd152a425e079503e29ded
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-02-18 16:35:14 -0500

    Update to determine which bar to launch based on active wm

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# kill any open running polybar instances
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar main-awesome &
+
+# lauch polybar for current desktop
+activeSessionId=$(for sessionid in $(loginctl list-sessions --no-legend | awk '{ print $1 }')
+  do loginctl show-session -p Id -p Name -p User -p State -p Type -p Remote $sessionid | sort
+  done |
+  awk -F = '/Id/ { id = $2 } /State/ { state = $2 } /Type/ { type = $2 } /Remote/ { remote = $2 } /User/ && remote == "no" && state == "active" && (type == "x11" || type == "wayland") { print id }')
+activeDesktop=$(loginctl show-session -p Desktop $activeSessionId | awk -F = '/Desktop/ { de = $2 } /Desktop/ { print de }')
+
+case "$activeDesktop" in
+  "leftwm")
+    polybar main-left &
+    ;;
+  "awesome")
+    polybar main-awesome &
+esac
